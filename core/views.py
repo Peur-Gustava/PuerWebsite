892ea8e46e1cf9edca16fe8544b55dd0dea0a198
kurianbenoy <kurian.pro@gmail.com>
2018-03-06 22:37:04 +0530

    Add a temporary task for form

--- core/views.py
+++ core/views.py
@@ -2,6 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import *
 from .forms import *
+from .tasks import add
+
+# add.delay(2,2)
+# core.tasks.add(2, 2)
+
 
 @login_required
 def FileUpload(request):
@@ -9,8 +14,11 @@ def FileUpload(request):
     if request.method == 'POST':
         form = Uploadform(request.POST,request.FILES)
         if form.is_valid():
-            # print(form.proof)
             form.save()
+
         else:
-            form = Uploadform()
+            form = Uploadform(commited=False)
+            data = form.cleaned_data
+
+            seriousmlprocess(data)
     return render(request,'file_upload.html',{'form':form })
--- shortcrux/settings.py
+++ shortcrux/settings.py
@@ -140,4 +140,3 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Africa/Nairobi'
