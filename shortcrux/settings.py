a8ec6003c098ab8cadeba401f22a67ec307a1348
kurianbenoy <kurian.pro@gmail.com>
2018-03-23 14:37:14 +0530

    Removed celery configuration from app

--- shortcrux/settings.py
+++ shortcrux/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'login',
-    'django_celery_beat',
-    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -132,11 +130,3 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_REDIRECT_URL = 'upload'
-
-
-# CELERY STUFF
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
